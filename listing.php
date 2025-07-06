8e8e4de1fd62eac2da377b61890f2824e6190bf9
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-07-06 08:41:29 +0500

    Added listing feature.

---
+++ listing.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listing</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+    <style>
+        body {
+            font-family: "Montserrat", sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+    // Read data from database
+    $dbConnection = mysqli_connect("localhost", "root", "", "company");
+
+    $readQuery = "SELECT * FROM employees";
+
+    $execQuery = mysqli_query($dbConnection, $readQuery);
+
+    while ($val = mysqli_fetch_assoc($execQuery)) {
+        echo $val['ID'] . " " . $val['firstName'];
+    }
+    ?>
+</body>
+
+</html>
