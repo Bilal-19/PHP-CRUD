07a7eebe941643a7b1f4474e3dfe329242137fd0
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-07-06 20:00:20 +0500

    Added action icons on listing page.

--- listing.php
+++ listing.php
@@ -11,6 +11,9 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
         body {
             font-family: "Montserrat", sans-serif;
@@ -39,6 +42,7 @@
                             <th>Age</th>
                             <th>Experience</th>
                             <th>Resume</th>
+                            <th>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -50,6 +54,12 @@
                             echo "<td> {$val['age']} </td>";
                             echo "<td> {$val['experience']} </td>";
                             echo "<td> {$val['resumePath']} </td>";
+                            echo
+                                "<td> 
+                                    <a class='text-success'><i class='fa fa-eye'></i></a>
+                                    <a class='mx-1 text-primary'><i class='fas fa-edit'></i></a>
+                                    <a class='text-danger'><i class='fa-solid fa-trash'></i></a>
+                            </td>";
                             echo "</tr>";
                         }
                         ?>
